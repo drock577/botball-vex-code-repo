b8d0c8fc1af5923cfa18403fd9a061db17203fdf
Dylan Lang <dylanlang999@gmail.com>
2016-03-22 12:34:08 -0700

    Random for motor movement test

--- botball/random.c
+++ botball/random.c
@@ -1,20 +1,24 @@
+#include <kipr/botball.h>
 #include<stdio.h>
-	#include<stdlib.h>
-	#include<time.h>
-
-	int main ()
-	{
-		int number, input;
-		/* initialize random seed: */
-		srand ( time(NULL) );
-		/* Generate a random number: */
-		number = rand() % 10 + 1;
-		do {
-				printf ("Guess the number (1 to 10): ");
-				scanf ("%d",&input);
-				if (number > input)
-					printf ("The number is higher\n");
-			} while (number!=input);
-		printf ("That is correct!\n");
-		return 0;
+#include<stdlib.h>	
+#include<time.h>
+	
+int main()
+{
+  	printf("random test");
+  	
+  	int randnum;
+	srand ( time(NULL) );
+	randnum = rand() % 2000 + 700;
+  	
+	enable_servos();
+	
+	for(x>10){
+  	wait_for_milliseconds(1000);
+  	set_servo_position(0,randnum);
+  	x++;
 	}
+	
+  	disable_servos();
+  	return 0;
+}
